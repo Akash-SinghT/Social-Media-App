add3d13032fa87009f69c3ab53adba338036fe08
Akash Singh <sakashh165@gmail.com>
2025-01-14 20:51:11 +0530

    Update RightSidebar.jsx

--- client/src/pages/RightSidebar.jsx
+++ client/src/pages/RightSidebar.jsx
@@ -1,31 +1,12 @@
 import React from "react";
 import Posts from "./Posts";
 import Feed from "./Feed";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Badge } from "lucide-react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 
 const RightSidebar = () => {
-  const { user } = useSelector((store) => store.auth);
   return (
-    <div className="w-fit my-10 pr-32">
-      <div className="flex items-center gap-2">
-        <Link to={`/profile/${user._id}`}>
-          <Avatar>
-            <AvatarImage src={user?.profilePicture} alt="post_image" />
-            <AvatarFallback className="bg-gray-800">CN</AvatarFallback>
-          </Avatar>
-        </Link>
-        <div>
-          <h1 className="font-semibold text-small">
-            <Link to={`/profile/${user._id}`}>{user?.username}</Link>
-          </h1>
-          <span className="text-white text-sm">
-            {user?.Bio || "Bio here.."}
-          </span>
-        </div>
-      </div>
+    <div>
+      SideBAr
+      <div>jsx</div>
     </div>
   );
 };
